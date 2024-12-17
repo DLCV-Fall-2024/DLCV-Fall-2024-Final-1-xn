444042bf2393232fa66588ecc97d2c08a9d987f7
HenryLin0822 <henrylin.0822@gmail.com>
2024-12-17 16:44:12 +0800

    local_evaluation_bash

--- llama_eval_local.sh
+++ llama_eval_local.sh
@@ -1,6 +1,6 @@
 python3 llama_eval_local.py \
     --model_id "meta-llama/Meta-Llama-3.1-8B-Instruct" \
-    --prediction "baseline_results/submission.json" \
+    --prediction "rag_results/submission.json" \
     --data_root "data" \
     --split "test" \
     --batch_size 1 \
