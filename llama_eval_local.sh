ad759978e9402f6b405dda4c10dc2a933181b5ac
HenryLin0822 <henrylin.0822@gmail.com>
2024-12-23 22:47:42 +0800

    delete files

--- llama_eval_local.sh
+++
@@ -1,8 +0,0 @@
-python3 llama_eval_local.py \
-    --model_id "meta-llama/Meta-Llama-3.1-8B-Instruct" \
-    --prediction "rag_results/submission.json" \
-    --data_root "data" \
-    --split "test" \
-    --batch_size 1 \
-    --temperature 0.1 \
-    --verbose
